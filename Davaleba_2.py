58e97a9a6a3213a00f6c520f00b9ce398ee04f56
Ivane <ivane@leavingstone.com>
2017-03-04 03:27:29 +0400

    New Task Solved!

---
+++ Davaleba_2.py
@@ -0,0 +1,32 @@
+
+
+
+def Calculus_Nums(number):
+
+    result = 0
+
+    if number % 2 == 0:
+        result = number/2
+    else:
+        result = number * 3 + 1
+    return result
+
+
+
+
+
+
+def main():
+
+   result = Calculus_Nums(5)
+
+   while result != 1:
+       print(result)
+       result = Calculus_Nums(result)
+       print(result)
+
+
+
+
+
+main()
