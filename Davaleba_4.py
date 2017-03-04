0cbdca7dea7a7d0c9b35bdbebedde3a71b599a74
Ivane <ivane@leavingstone.com>
2017-03-04 13:45:53 +0400

    4 Davalebac Done

---
+++ Davaleba_4.py
@@ -0,0 +1,52 @@
+import sys
+
+
+
+
+def get_number():
+    print('hi')
+
+
+
+
+def main():
+
+
+
+    def hello():
+
+
+        var = 1
+
+        var_list = []
+
+
+
+        while True:
+            var = input("Es programa poulobs udides da umceres ricxvebs. \nSheikvane ricxvi: ")
+            var = int(var)
+
+            if var == 0:
+                print("fuck off")
+                break
+            else:
+
+                var_list.append(int(var))
+                print(var_list)
+
+
+
+                max_list = max(var_list)
+                min_list = min(var_list)
+
+                print("Minimaluri: ", min_list)
+                print("Maximaluri: ", max_list)
+
+
+
+    hello()
+
+
+
+
+main()
