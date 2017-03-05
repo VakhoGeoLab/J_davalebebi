5d0bc975c10d4d3994db7983969370775893f8bb
VakhoGeoLab <ivane@leavingstone.com>
2017-03-06 00:15:56 +0400

    Beatiful code change git Username

---
+++ Davaleba_5.py
@@ -0,0 +1,25 @@
+
+def acronym(string):
+    word_list = string.split()
+    #print(word_list)
+
+
+    char_list = []
+
+    for item in word_list:
+        char_list.append(item[0])
+
+    acro = "".join(char_list)
+    print(acro)
+
+
+
+def main():
+    #print("hello world")
+
+    sentence = "trakshi pari"
+
+    acronym(sentece)
+
+
+main()
